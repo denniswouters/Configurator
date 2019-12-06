f459bfe522af457f4d470227e99ec0fcd4aece2e
Dennis Wouters <denniswouters@192.168.0.106>
2019-12-06 10:25:19 +0100

    Front-end dashboard

---
+++ dashboard/dashboard.php
@@ -0,0 +1,205 @@
+<?php include 'header.php';?>
+
+    <div class="container">
+    <div class="row">
+        <div class="dragmenu col-lg-4">
+                <div class="row">
+                  <div class="col-lg-6">
+                            <div class="dragmenuItem">
+                                    <img src="img/dots.png" class="dots center">
+                                    <p>Introductie</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                            <div class="dragmenuItem">
+                            <img src="img/dots.png" class="dots center">
+                            <p>Introductie</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                            <div class="dragmenuItem">
+                            <img src="img/dots.png" class="dots center">
+                            <p>Introductie</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                            <div class="dragmenuItem">
+                            <img src="img/dots.png" class="dots center">
+                            <p>Introductie</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                            <div class="dragmenuItem">
+                            <img src="img/dots.png" class="dots center">
+                            <p>Introductie</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                <div class="col-lg-6">
+                        <div class="dragmenuItem">
+                        <img src="img/dots.png" class="dots center">
+                        <p>Introductie</p>
+                        </div>
+                </div>
+                </div> 
+                </div>
+
+                <div id="overlay" onclick="off()"></div>
+
+                        <div class="col-lg-7 offset-lg-1">
+                                    <div class="row">
+                                        <div class="page col-lg-3">
+                                            <div class="page-label">
+                                                Homepagina
+                                            </div>
+                                       </div>  
+                                       <div class="addPage col-lg-3 offset-lg-1">
+                                            <div class="addPageText">
+                                                +
+                                            </div>
+                                       </div>
+                                    </div>
+                                    <div class="mt-5">
+                                    <div class="row">
+                                        <div class="button col-lg-1 offset-lg-7">
+                                            <div class="multipage" onclick="on()">
+                                                <img src="img/multi.png" class="multi">
+                                            </div>
+                                        </div>
+                                        <div class="button col-lg-3 offset-lg-0">
+                                            <div class="export">
+                                                Exporteren
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+
+        
+        
+                            </div>
+    
+    
+    
+    </div>
+
+<?php include 'footer.php';?>
---
+++ dashboard/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Orbitron:400,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="../main.js"></script>
+</head>
+<body>
+    <div class="header">
+        <img src="img/corner.png" class="corner">
+        <img src="img/companyfuel.png" class="logo">
+    </div>
